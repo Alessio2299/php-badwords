76ecd60d9a1d15786a4c6c21a3fe8b06e9f34d27
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-09 15:27:13 +0100

    Fixed Text

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
   $wordToBeCensored = $_GET["word"];
-  $text = "First try with PHP";
+  $text = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea omnis eveniet consequatur vel, aperiam iste numquam aliquam sequi quibusdam illum nisi repellat placeat quas ratione error itaque dicta voluptatum consequuntur!";
   $textLength = strlen($text);
   $textReplace = str_ireplace($wordToBeCensored, '***' , $text);
   $textReplaceLength = strlen($textReplace);
 
 ?>
-
-<h2><?php echo $text ?></h2>
+<h2>Uncensored Text</h2>
+<p><?php echo $text ?></p>
 <p>The length of this string is: <?php echo $textLength ?></p>
 <hr>
-<h2><?php echo $textReplace ?></h2>
+<h2>Censored Text</h2>
+<p><?php echo $textReplace ?></p>
 <p>The length of this string is: <?php echo $textReplaceLength ?></p>
 
