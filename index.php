6eca053df9b18694840c342c1d79dafd83873050
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-10 10:36:30 +0100

    Fix Exercise

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
   $textReplaceLength = strlen($textReplace);
 
 ?>
-<h2>Uncensored Text</h2>
-<p><?php echo $text ?></p>
-<p>The length of this string is: <?php echo $textLength ?></p>
-<hr>
-<h2>Censored Text</h2>
-<p><?php echo $textReplace ?></p>
-<p>The length of this string is: <?php echo $textReplaceLength ?></p>
-
+<!DOCTYPE html>
+  <html lang="it">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+  </head>
+  <body>
+    <h2>Uncensored Text</h2>
+    <p><?php echo $text ?></p>
+    <p>The length of this string is: <?php echo $textLength ?></p>
+    <hr>
+    <h2>Censored Text</h2>
+    <p><?php echo $textReplace ?></p>
+    <p>The length of this string is: <?php echo $textReplaceLength ?></p>
+  </body>
+</html>
